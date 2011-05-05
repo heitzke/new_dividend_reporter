ce184546a4fb3a5347b9cf5d0dd48e296674333b
Allan Michaels <allan@isotope11.com>
2011-05-05 16:34:06 -0500

    created project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewDividendReporter::Application.config.secret_token = '86842477748d6022e9d7ef3ebb59b282285a3c8dfdb9651f9c9c5a7192aa9991d29c0df163e06edfcc153180bebe8c781ee20b62529397674d5756bc410ace6d'
