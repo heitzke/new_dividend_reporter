98c244abbab1d682af011d39329e62e92ed3ded4
Allan Michaels <allan@isotope11.com>
2011-05-09 14:41:05 -0500

    removed redundant devise tests

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,3 +1,13 @@
 Given /^I am on the login page$/ do
   visit new_user_session_path
 end
+
+Given /^I am not logged in$/ do
+  current_user = nil
+end
+
+Given /^I am on the create account page$/ do
+  visit new_user_registration_path 
+end
+
+
