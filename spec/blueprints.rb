254f4d6355aa91338bd5cf49469d3455c4e260f7
Allan Michaels <allan@isotope11.com>
2011-05-09 10:14:41 -0500

    added stock sham to spec/blueprints.rb

--- spec/blueprints.rb
+++ spec/blueprints.rb
@@ -1,2 +1,10 @@
 require 'machinist/active_record'
 require 'sham'
+require 'faker'
+
+Sham.define do
+  # Stock
+  company_name                    { |index| index.to_s                }
+  # Ticker
+  ticker                          { |index| index.to_s(0..3)          }
+end
