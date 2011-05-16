18ac6fa0c6d1281b84c4f8ed407be9815b54c222
Allan Michaels <allan@isotope11.com>
2011-05-15 20:04:48 -0500

    [LIB] completed get_quotes method in yahoo_integrator

--- lib/yahoo_integrator.rb
+++ lib/yahoo_integrator.rb
@@ -1,25 +1,16 @@
 class YahooIntegrator
   attr_accessor :stocks, :quotes
-
-  def initialize(options={})
-    @stocks = options[:stocks]
-  end
-
-  def run
-    get_latest_quotes @stocks
-    save_quotes @quotes
-  end
-
-protected
-  def get_latest_quotes(stocks)
-    # This should hit YahooFinance api and get the quote info
-    @quotes = YahooFinance.get_standard_quotes(stocks)
-  end
-
-  def save_quotes(quotes) 
-    # This should create new entries for the quotes
-    quotes.each do |quote|
-      Quote.create(:dividend_pay_date => dividendPayDate, )
+  def get_quotes stock_array
+    yahoo_hash = YahooFinance.get_standard_quotes stock_array
+    yahoo_hash.keys.each do |ticker|
+      stock = Stock.find_by_ticker ticker
+      quote = Quote.new :stock_id => stock.id
+      quote.update_attributes(    :ex_dividend_date       =>   yahoo_hash[ticker].exDividendDate,
+                                  :dividend_pay_date      =>   yahoo_hash[ticker].dividendPayDate,
+                                  :dividend_yield         =>   yahoo_hash[ticker].dividendYield,
+                                  :dividends_per_share    =>   yahoo_hash[ticker].dividendPerShare,
+                                  :last_price             =>   yahoo_hash[ticker].lastTrade )
+      quote.save
     end
   end
 end
